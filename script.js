233596240037e3378c85117f3991dfc4faee929d
Alexig2 <alex4342on@gmail.com>
2024-10-11 15:57:15 -0400

    Geral

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let resultado = document.getElementById('resultado');
 let resultadoEx3 = document.getElementById('resultadoEx3');
 
 //Exercício 2
-function somar(){
+function somar() {
     let n1 = parseFloat(document.getElementById("n1").value);
     let n2 = parseFloat(document.getElementById("n2").value);
     let soma = n1 + n2;
@@ -16,10 +16,10 @@ btnMedia.addEventListener('click', () => {
     let n3 = parseFloat(document.getElementById("nota3").value);
     let n4 = parseFloat(document.getElementById("nota4").value);
 
-    let media = (n1 + n2 + n3 + n4)/4;
+    let media = (n1 + n2 + n3 + n4) / 4;
 
     resultadoEx3.innerHTML = `Média: ${media}`;
-    if(media > 7){
+    if (media > 7) {
         statusEx3.innerHTML = `Status: Aprovado`;
     } else {
         statusEx3.innerHTML = `Status: Reprovado`;
@@ -27,3 +27,27 @@ btnMedia.addEventListener('click', () => {
 });
 
 //Exercício 5
+btnTabuada.addEventListener('click', () => {
+    let n = parseFloat(document.getElementById('nEx5').value);
+    document.getElementById('resultadoEx5').innerHTML = geraTabuada(n)
+})
+
+function geraTabuada(n) {
+    let tabuada = ''
+    for (i = 0; i < 11; i++) {
+        tabuada += `${n} x ${i} = ${n * i}<br>`
+    }
+    return tabuada
+}
+
+//Exercício 6
+let maior = -1
+btnLerMaior.addEventListener('click', () => {
+    let n = parseInt(document.getElementById('nEx6').value)
+
+    if(n > maior){
+        maior = n
+    }
+
+    document.getElementById('resultadoEx6').innerHTML = `Maior número: ${maior}`
+})
